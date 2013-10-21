a19a2b718a5d4d8959564871b616166e003ff2eb
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2013-10-21 18:05:25 -0400

    Update the application's style

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,8 +2,30 @@
 // TODO: Caching for most remote data (prepopulate on first connect then update every X or forced)
 // TODO: Support pagination
 
-if (typeof require === 'function') {
+var in_node_webkit = (typeof require === 'function');
+
+if (in_node_webkit) {
 	var lodash = require('lodash');
+	var gui = require('nw.gui');
+
+	var tray = new gui.Tray({ icon: 'assets/images/tray_icon.png' });
+
+	tray.on('click', function() {
+		gui.Window.get().show();
+	});
+
+
+	var hot_key = new gui.HotKey({
+	  key: "Alt+R",
+	  activated: function() {
+	    console.log("activated");
+	  },
+	  failed: function(msg) {
+	    console.log(msg);
+	  }
+	});
+
+	gui.App.registerGlobalHotKey(hot_key);
 }
 
 var projects = [];
@@ -50,6 +72,9 @@ var fetch_project_tracker = function(identifier) {
 };
 
 var submit_issue = function(project_id, tracker_id, subject, description) {
+	$('#pin').addClass('hidden');
+	$('#loading').removeClass('hidden');
+
 	$.ajax({
 		type: 'POST',
 		url: redmine_site+'/issues.json',
@@ -63,13 +88,22 @@ var submit_issue = function(project_id, tracker_id, subject, description) {
 			}
 		}
 	}).done(function() {
-		$('#message').addClass('alert alert-success').html('Success :D');
+		//$('#message').addClass('alert alert-success').html('Success :D');
+
+		if (event.keyCode === 27 && in_node_webkit) {
+			gui.Window.get().show();
+		}
 	}).fail(function() {
 		$('#message').addClass('alert alert-danger').html('Error T_T');
+	}).complete(function() {
+		$('#loading').addClass('hidden');
+		$('#pin').removeClass('hidden');
 	});
 };
 
 $(document).ready(function() {
+	$('#subject').focus();
+
 	fetch_projects();
 
 	$('select[data-chosen]').chosen();
@@ -93,4 +127,95 @@ $(document).ready(function() {
 			fetch_project_tracker(project.identifier);
 		}
 	});
+
+
+	// cmd + enter = submit
+	$('form').keydown(function(event) {
+		if (event.keyCode === 13 && event.metaKey) {
+			$(this).submit();
+			return false;
+		}
+	});
+
+	// esc
+	$(window).keydown(function(event) {
+		if (event.keyCode === 27 && in_node_webkit) {
+			gui.Window.get().hide();
+		}
+	});
+
+	// Autogrow textarea
+	$('textarea').autogrow();
+
+	// Blur
+	/*if (in_node_webkit) {
+		gui.Window.get().on('blur', function() {
+			this.hide();
+		});
+	}*/
 });
+
+
+
+//==============================================================================
+
+(function($)
+{
+    /**
+     * Auto-growing textareas; technique ripped from Facebook
+     *
+     * http://github.com/jaz303/jquery-grab-bag/tree/master/javascripts/jquery.autogrow-textarea.js
+     */
+    $.fn.autogrow = function(options)
+    {
+        return this.filter('textarea').each(function(options) {
+            var self         = this;
+            var $self        = $(self);
+            var minHeight    = $self.height();
+            var noFlickerPad = $self.hasClass('autogrow-short') ? 0 : parseInt($self.css('lineHeight')) || 0;
+
+            var shadow = $('<div></div>').css({
+                position:    'absolute',
+                top:         -10000,
+                left:        -10000,
+                width:       $self.width(),
+                fontSize:    $self.css('fontSize'),
+                fontFamily:  $self.css('fontFamily'),
+                fontWeight:  $self.css('fontWeight'),
+                lineHeight:  $self.css('lineHeight'),
+                resize:      'none',
+                'word-wrap': 'break-word'
+            }).appendTo(document.body);
+
+            var update = function(event)
+            {
+                var times = function(string, number)
+                {
+                    for (var i=0, r=''; i<number; i++) r += string;
+                    return r;
+                };
+
+                var val = self.value.replace(/</g, '&lt;')
+                                    .replace(/>/g, '&gt;')
+                                    .replace(/&/g, '&amp;')
+                                    .replace(/\n$/, '<br/>&nbsp;')
+                                    .replace(/\n/g, '<br/>')
+                                    .replace(/ {2,}/g, function(space){ return times('&nbsp;', space.length - 1) + ' ' });
+
+                // Did enter get pressed?  Resize in this keydown event so that the flicker doesn't occur.
+                if (event && event.data && event.data.event === 'keydown' && event.keyCode === 13) {
+                    val += '<br />';
+                }
+
+                shadow.css('width', $self.width());
+                shadow.html(val + (noFlickerPad === 0 ? '...' : '')); // Append '...' to resize pre-emptively.
+                $self.height(Math.max(shadow.height() + noFlickerPad, minHeight));
+            }
+
+            $self.change(update).keyup(update).keydown({event:'keydown'},update);
+            $(window).resize(update);
+
+            update();
+        });
+    };
+})(jQuery);
